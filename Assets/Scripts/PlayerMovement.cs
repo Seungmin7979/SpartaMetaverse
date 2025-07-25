781cd6237a578d9aa935dc0e2d8722719a52a243
Seungmin7979 <safetmdals79@gmail.com>
2025-07-25 14:22:01 +0900

    feat : 카메라 및 플레이어 맵 이탈 불가 구현

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 5f;
+
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    private void Awake()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        // 1. 이동 입력
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        // 평면 이동
         rb.velocity = movement * moveSpeed;
+    }
+
+    void LateUpdate()
+    {
+        // 캐릭터 벽 안나가게 설정
+        Vector2 pos = transform.position;
+
+        pos.x = Mathf.Clamp(pos.x, -17.6f, 21.6f);    // X 경계
+        pos.y = Mathf.Clamp(pos.y, -9.2f, 12.9f);    // Y 경계
+
+        transform.position = pos;
     }
 }
