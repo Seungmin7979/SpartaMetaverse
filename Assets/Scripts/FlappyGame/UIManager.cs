c68d4fc56a378b309922cd94e931f2afc9e76ebd
Seungmin7979 <safetmdals79@gmail.com>
2025-07-27 15:50:43 +0900

    Feat, Refactor : 최고 기록 UI 추가 및 겹치는 코드 삭제, 간략화

--- Assets/Scripts/FlappyGame/UIManager.cs
+++ Assets/Scripts/FlappyGame/UIManager.cs
@@ -9,17 +9,24 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI restartText;
     public Button backToMain;
+    public TextMeshProUGUI bestScoreText;
+
+    private int bestScore = 0;
+    private const string BestScoreKey = "BestScore";
 
     // Start is called before the first frame update
     void Start()
     {
-        if (restartText == null)
-            Debug.LogError("restart text is null");
+        if (restartText == null) Debug.LogError("restart text is null");
 
-        if (scoreText == null)
-            Debug.LogError("score text is null");
+        if (scoreText == null) Debug.LogError("score text is null");
+
+        if (bestScoreText == null) Debug.LogError("bestScore text is null");
 
         restartText.gameObject.SetActive(false);
+
+        bestScore = PlayerPrefs.GetInt(BestScoreKey, 0);
+        BestScore(bestScore);
     }
 
     public void SetRestart()
@@ -30,10 +37,23 @@ public class UIManager : MonoBehaviour
     public void UpdateScore(int score)
     {
         scoreText.text = score.ToString();
+
+        if (score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt(BestScoreKey, bestScore);
+            PlayerPrefs.Save();
+            BestScore(bestScore);
+        }
     }
 
     public void BackToMain()
     {
         backToMain.gameObject.SetActive(true);
+    }
+
+    public void BestScore(int bestScore)
+    {
+        bestScoreText.text = bestScore.ToString();
     }
 }
--- Assets/Scripts/SpartaMetaverse/StartMiniGame.cs
+++ Assets/Scripts/SpartaMetaverse/StartMiniGame.cs
@@ -8,6 +8,25 @@ public class StartMiniGame : MonoBehaviour
     public string minigameSceneName = "FlappyGameScene";
     public GameObject interactionUI;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactionUI.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (interactionUI != null)
+            {
+                interactionUI.SetActive(false);
+            }
+        }
+    }
+
     public void StartMinigameScene()
     {
         SceneManager.LoadScene(minigameSceneName);
